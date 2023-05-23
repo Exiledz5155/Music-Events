d9d1c1f1c7f17de83e6d7f4ef7eb0f865afb21d2
Danny Bui <Dannybui5155@gmail.com>
2023-05-23 13:29:52 -0500

    Implemented send email function and while loop for checking events

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 import selectorlib
+import smtplib, ssl
+import os
+import time
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
 
@@ -17,9 +20,20 @@ def extract(source):
     return value
 
 
-def send_email():
-    """Sends the event as an email"""
-    print("Email was sent!")
+def send_email(message):
+    """Sends an email"""
+    host = "smtp.gmail.com"
+    port = 465
+    username = "Dannybui5155@gmail.com"
+    with open(".env", 'r') as file:
+        password = file.read().split("=")[1]
+        # print(password)
+    receiver = "Dannybui5155@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host=host, port=port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
 
 def store(extracted):
@@ -35,13 +49,16 @@ def read(extracted):
 
 
 if __name__ == "__main__":
-    scraped = scrape(URL)
-    extracted = extract(scraped)
-    print(extracted)
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
 
-    content = read(extracted)
-    # If event exists and not a duplicate, send the email and store in txt
-    if extracted != "No upcoming tours":
-        if extracted not in content:
-            store(extracted)
-            send_email()
+        content = read(extracted)
+        # If event exists and not a duplicate, send the email and store in txt
+        if extracted != "No upcoming tours":
+            if extracted not in content:
+                store(extracted)
+                send_email(message="Hey, a new event was found!")
+                print("Email was sent!")
+        time.sleep(300) # Checks for events every 5 minutes
